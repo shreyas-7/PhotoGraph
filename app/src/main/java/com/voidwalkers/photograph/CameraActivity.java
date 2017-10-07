3be5607ca32a72a1ddf94e341ff85cbc484df4b7
PIMPALGAONKAR SHREYAS <shreyas@cse.iitb.ac.in>
2017-10-07 20:22:08 +0530

    Update1

---
+++ app/src/main/java/com/voidwalkers/photograph/CameraActivity.java
@@ -0,0 +1,34 @@
+package com.voidwalkers.photograph;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class CameraActivity extends FragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(!MarshmallowPermissions.checkPermissionForCamera(this)) {
+            MarshmallowPermissions.requestPermissionForCamera(this);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+}
